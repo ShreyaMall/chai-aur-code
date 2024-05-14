f6b0303b9de905d8b0ee8049b89225c9df6c7787
ShreyaMall <140044692+ShreyaMall@users.noreply.github.com>
2024-05-14 11:58:04 +0000

    Datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(4
+    +4)
+
+console.log("Shreya");
+
+let name = "shreya"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => True/False
+// null => standloan Value
+// undefined =>
+// symbol => unique
+
+// object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
