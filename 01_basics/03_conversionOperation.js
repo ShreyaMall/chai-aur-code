a5f561b6691b5234371b3eeb144586104d11fe35
ShreyaMall <140044692+ShreyaMall@users.noreply.github.com>
2024-05-23 11:45:04 +0000

    cofig done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,47 @@
+let score = false
+
+//console.log(typeof score);
+
+//console.log(typeof score )
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// 33 => 33 //
+
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber); 
+//console.log(typeof stringNumber);
+
+
+
+
+
+
+//***************** Operatins ******************//
+
+let value = 8
+let negValue = -value
+console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " Shreya"
+
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+
