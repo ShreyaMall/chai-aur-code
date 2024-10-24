d358e37546e009aefd88c20d740e08bfb95c2291
ShreyaMall <140044692+ShreyaMall@users.noreply.github.com>
2024-10-24 06:06:43 +0000

    Datatypes summary

---
+++ Datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive data types
+
+// 7 Types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+//const bigNumber = 2424242424242424n
+
+
+// Reference (Non primitive)
+
+
+// Array, Objects, Functions
+
+const heros = ["LightYagami","Ryuk", "Misa"]
+let myObj = {
+    name: "Shreya",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof bigNumber);
